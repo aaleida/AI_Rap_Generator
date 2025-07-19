c736861be554cb29eef0dd13cb30d975bf58c548
aaleida <alena.aleida@gmail.com>
2025-07-19 23:05:24 +0200

    API
    created API structure.

--- src/script.js
+++ src/script.js
@@ -1,15 +1,23 @@
-function generatePoem(event) {
-    event.preventDefault();
-
+function displayPoem(response) {
     new Typewriter("#poem", {
-        strings: ["Je marcherai les yeux fixés sur mes pensées, Sans rien voir au dehors, sans entendre aucun bruit, Seul, inconnu, le dos courbé, les mains croisées, Triste, et le jour pour moi sera comme la nuit."],
+        strings: response.data.answer,
         autoStart: true,
-        loop: false,
         delay: 20,
         cursor: "",
     });
 }
 
+function generatePoem(event) {
+    event.preventDefault();
+
+    let apiKey = "a9bda932f2ao07463160563ed2640t39";
+    let userInput = document.querySelector("#prompt").value;
+    let prompt = `Generate a French poem about ${userInput}`;
+    let context = `Generate a French poem based on the theme: ${userInput}. The poem should be short, creative, and capture the essence of the theme.`;
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(displayPoem);
+}
+
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
-
